ff1e64c4688ea10ea0bf4f8e560585b72818a9a7
Freitas29 <arao.freitas@hotmail.com>
2019-09-20 11:35:42 -0300

    test to create a user

---
+++ test/functional/register.spec.js
@@ -0,0 +1,17 @@
+const { test, trait } = use('Test/Suite')('User registeration')
+const Factory = use('Factory')
+
+trait('Test/ApiClient')
+
+test('it should return a user with success', async ({ assert, client }) => {
+    const data = {
+        username: "teste",
+        email: "teste@email.com",
+        password: "password"
+    }
+
+    const response = await client.post('/users').send(data).end()
+
+    response.assertStatus(200)
+    assert.equal(data.email,response.body.email)
+})
